700cb05fa79ac4283ff192c6d4d0d0d31984d725
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-05-13 00:49:05 -0700

    troubleshoot js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,34 @@
-var setDate = function () {
-    $("#currentDay").text(moment().format("dddd, MMMMM Do YYYY, h:mm:ss a"));
+var currentDate = function () {
+    $("#thisDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    
 }
+var timeBlock = [
+    "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"
+]
 
-var currentHour = ["9:00 AM", "10:00 AM", "11:00 AM","12:00 AM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM","6:00 PM"]
 
 $(".saveBtn").on('click', function () {
-    console.log("Clicked save button", $(this));
+    console.log("Clicked save button ", $(this));
+    //grab the hour and description when the save button is clicked 
+    var timeStaple = $(this).siblings('.hour').text();
+    console.log(timeStaple);
+    var description = $(this).siblings('.description').val();
+    console.log(description);
 
-    var timeStamp = $(this).siblings('.hour').text();
-    console.log(timeStamp);
-
-    var sectionDescription = $(this).siblings('.description').val();
-    console.log(sectionDescription);
-
-    localStorage.setItem(timeStamp, sectionDescriptio);
+    //save information in localStorage 
+    localStorage.setItem(timeStaple, description);
 });
 
-for (let i = 0; i < currentHour.length; i ++) {
-    console.log("timeStamp", currentHour[i])
 
-    var id = "#" + currentHour[i].split(":")[0];
+for (let i = 0; i < timeBlock.length; i++) {
+    
+    var id = "#" + timeBlock[i].split(":")[0];
+    //console.log("id", id )
+    $(id).val(localStorage.getItem(timeBlock[i]))
 
-    $(id).val(locaStorage.getItem(currentHour[i]))
 }
 
 
 
-setDate();
+//call function
+currentDate(); 
